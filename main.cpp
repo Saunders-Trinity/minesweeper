b67669f970f1cad4ee57b7a7a181c4fa27062636
Saunders-Trinity <trinity.saunders@hotmail.com>
2024-04-20 23:26:00 -0400

    The game is working at this point. Only things to do are get number tiles UNDER the tiles. fix the welcome screen and finishing the leaderboard screen.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 #include "window.h"
 #include <SFML/Graphics.hpp>
+#include "random"
+#include <cstdlib>
+#include <ctime>
 
 
 
@@ -38,8 +41,7 @@ int main()
     int rowCount = 0;
     int mineCount = 0;
     ifstream config("files/config.cfg");
-    if(config.is_open()){//25,16,50
-        //cout << "config open successful" << endl;
+    if(config.is_open()){
         config >> colCount;
         config >> rowCount;
         config >> mineCount;
@@ -81,8 +83,6 @@ int main()
     text2.setStyle(sf::Text::Bold | sf::Text::Regular);
     text2.setPosition(450,325);
 
-
-
     while (window.isOpen())//leaderboard goes in while loop
     {
         sf::Event event;
@@ -116,10 +116,7 @@ int main()
 //start of game screen drawing
     }
 
-    sf::RenderWindow gameWindow(sf::VideoMode(colCount*32, rowCount*32 + 100), "MINESWEEPER",sf::Style::Close);
-    int windowWidth = colCount * 32;
-    int windowHeight = rowCount * 32 + 100;
-
+    sf::RenderWindow gameWindow(sf::VideoMode(colCount*32, rowCount*32 + 100), "MINESWEEPER!",sf::Style::Close);
 
     sf::Texture texture1;//this is the tile hidden texture
     texture1.loadFromFile("files/images/tile_hidden.png");
@@ -142,71 +139,202 @@ int main()
 
     std::vector<sf::Vector2f> leftClickPositions; //this is for tile revealed positions to be stored
 
-//for mine to loaded on screen
+//loading mine sprites
     sf::Texture texture4;
     texture4.loadFromFile("files/images/mine.png");
     sf::Sprite spriteMine;
     spriteMine.setTexture(texture4);
+//loading timers
 
-//starting implementation of mines
-    bool minesGenerated = false;
+
+
+
+
+
+
+    //for tile implementation, this helps with tile generation do not delete
+    vector<vector<Tile >> TileVector;//vector to hold sprites
+    for (int x = 0; x < colCount; ++x) {//draws screen full hidden tiles
+        vector<Tile> innerTiles;
+        for (int y = 0; y < rowCount; ++y) {
+            Tile temp = Tile(x * 32, y * 32);
+
+            innerTiles.push_back(temp);
+//            cout << x << " "<<  y << endl;
+
+        }
+        TileVector.push_back(innerTiles);
+    }
+
+
+    sf::Texture debugTexture;
+    debugTexture.loadFromFile("files/images/debug.png");
+    sf::Sprite debugSprite;
+    debugSprite.setTexture(debugTexture);
+
+    sf::Texture happyTexture;
+    happyTexture.loadFromFile("files/images/face_happy.png");
+    sf::Sprite happySprite;
+    happySprite.setTexture(happyTexture);
+
+    sf::Texture playTexture;
+    playTexture.loadFromFile("files/images/play.png");
+    sf::Sprite playSprite;
+    playSprite.setTexture(playTexture);
+
+    sf::Texture leaderboardTexture;
+    leaderboardTexture.loadFromFile("files/images/leaderboard.png");
+    sf::Sprite leaderboardSprite;
+    leaderboardSprite.setTexture(leaderboardTexture);
+
+    sf::Texture pauseTexture;
+    pauseTexture.loadFromFile("files/images/pause.png");
+
+
+    //This  helps generates the mines/ DO NOT DELETE
+    TileVector[0][0].generateMines(gameWindow, TileVector, mineCount);
+
+
+
+
 
 
 
     while (gameWindow.isOpen())
     {
+
         sf::Event event;
-        int repetitionsX = windowWidth / texture1.getSize().x + 1;
-        int repetitionsY = windowHeight / texture1.getSize().y + 1;
-        while (gameWindow.pollEvent(event)){
-            if (event.type == sf::Event::Closed){
+        while (gameWindow.pollEvent(event)) {
+            sf::Vector2i mousePosition = sf::Mouse::getPosition(gameWindow);
+            if (event.type == sf::Event::Closed) {
                 gameWindow.close();
             }
-        }
-        gameWindow.clear(sf::Color(128, 128, 128, 128));
-        gameWindow.draw(spriteHidden);//draws one sprite
-        for (int x = 0; x < repetitionsX; ++x) {//draws screen full hidden tiles
-            for (int y = 0; y < repetitionsY; ++y) {
-                spriteHidden.setPosition(x * texture1.getSize().x, y * texture1.getSize().y);
-                gameWindow.draw(spriteHidden);
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Right) {
+                //places a flag on top of tile hidden - this works
+                if (!TileVector[mousePosition.x / 32][mousePosition.y / 32].isRevealed) {
+                    TileVector[mousePosition.x / 32][mousePosition.y / 32].isFlag = !TileVector[mousePosition.x / 32][
+                            mousePosition.y / 32].isFlag;
+                }
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left ) {
+                //left click does a tile revealed action
+                TileVector[mousePosition.x / 32][mousePosition.y / 32].reveal();
+                //for button clicks
+                sf::FloatRect debugButtonBounds = debugSprite.getGlobalBounds();
+                sf::FloatRect faceButtonBounds = happySprite.getGlobalBounds();
+                sf::FloatRect pauseButtonBounds = playSprite.getGlobalBounds();
+                sf::FloatRect leaderboardButtonBounds = leaderboardSprite.getGlobalBounds();
+
+                if(debugButtonBounds.contains(sf::Vector2f(mousePosition))){//all mines reveal
+                        cout << "debug button clicked" << endl;
+                        TileVector[0][0].revealallMines(gameWindow,TileVector);
+                    }
+                else if(faceButtonBounds.contains(sf::Vector2f(mousePosition))){
+                        //this is for the happy face button
+                        //suposed to start game from beginning
+                        cout << "happy face clicked" << endl;
+                        happyTexture.loadFromFile("files/images/face_lose.png");
+                        happySprite.setTexture(happyTexture);
+
+                    }
+                else if(pauseButtonBounds.contains(sf::Vector2f(mousePosition))){
+                        cout << "pause button clicked" << endl;
+                        //this is for the pause/play sprite
+                        //pauses the time
+                        //flips the pause button to play button
+                        playSprite.setTexture(pauseTexture);
+
+                    }
+                else if(leaderboardButtonBounds.contains(sf::Vector2f(mousePosition))) {
+                    //window.clear and goes the leader board
+                        cout << "leaderboard button clicked" << endl;
+                        gameWindow.close();
+                        sf::RenderWindow leaderboardWindow(sf::VideoMode(800, 600), "Leaderboard");
+                        sf::Text leaderboardText;
+                        leaderboardText.setFont(font);
+                        leaderboardText.setCharacterSize(20);
+                        leaderboardText.setFillColor(sf::Color::White);
+                        leaderboardText.setStyle(sf::Text::Bold | sf::Text::Underlined);
+                        leaderboardText.setPosition(300,280);
+                        leaderboardText.setString("LEADERBOARD");
+
+
+                        while (leaderboardWindow.isOpen()) {
+
+
+                            while (leaderboardWindow.pollEvent(event)) {
+                                if (event.type == sf::Event::Closed) {
+                                    leaderboardWindow.close();
+                                }
+                            }
+                            leaderboardWindow.clear(sf::Color::Blue);
+                            // Draw your leaderboard content here
+                            vector<Player> leaderboard = Player::readLeaderboardFromFile("leaderboard.txt");
+                            Player::displayLeaderboard(leaderboard);
+
+                            leaderboardWindow.draw(leaderboardText);
+                            leaderboardWindow.display();
+
+
+                        }
+                }
+            }
+
+
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left ){
+
+                //if the user left clicks and it is a mine tile
+                //reveals all other mine function
+                int x = event.mouseButton.x / 32;
+                int y = event.mouseButton.y / 32;
+
+                if (TileVector[x][y].isMine) {//if user clicks and it is a mine
+                    TileVector[x][y].getAdjacentMines(gameWindow,TileVector, rowCount, colCount);//this works
+                    TileVector[x][y].revealallMines(gameWindow,TileVector);//this works
+                    TileVector[x][y].revealallTiles(gameWindow,TileVector);//does not work
+                    }
+
             }
         }
-        if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Right)
-        {
-            //place a flag on top of tile hidden
-            sf::Vector2i mousePosition = sf::Mouse::getPosition(gameWindow);
-            rightclickPosition = gameWindow.mapPixelToCoords(mousePosition);
-            rightclickPositions.push_back(rightclickPosition);
+
+
+
+
+
+
+        //for buttons and their positions, do not delete
+        debugSprite.setPosition(496,528);
+        happySprite.setPosition(368,528);
+        playSprite.setPosition(560,528);
+        leaderboardSprite.setPosition(624,528);
+
+        gameWindow.clear(sf::Color(255, 255, 255, 128));
+
+
+
+        //this draws tiles, DO NOT DELETE
+        //two for loops iterate through tile vectors and use draw function on both
+        for(int i = 0; i < TileVector.size(); i++){
+            for(int j = 0; j < TileVector[i].size(); j++){
+                TileVector[i][j].drawTile(gameWindow);
+                int count = TileVector[i][j].getAdjacentMines(gameWindow, TileVector, i, j);//count is right
+                TileVector[i][j].revealNumbers(gameWindow,count,TileVector,i,j);
+            }
+
         }
 
 
-        for (const auto& pos : rightclickPositions) {//this draws multiple flags in the spots where the user clicks
-            spriteFlag.setPosition(pos);
-            gameWindow.draw(spriteFlag);
-        }
 
-        if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left){
-            //left click does a tile revealed action
-            sf::Vector2i mousePosition = sf::Mouse::getPosition(gameWindow);
-            sf::Vector2f clickPosition = gameWindow.mapPixelToCoords(mousePosition);
-            leftClickPositions.push_back(clickPosition);//saves where the user clicks
-            spriteRevealed.setPosition(clickPosition);
-
-
-
-
-        }
-
-        /*Tile tile1(true);
-        Tile tile2(false);
-        tile2.setAdjacentMines(2);//this puts 2 mines adjacent of this tile
-        tile2.reveal();
-        tile2.toggleFlag();*/
-
+        gameWindow.draw(leaderboardSprite);
+        gameWindow.draw(playSprite);
+        gameWindow.draw(happySprite);
+        gameWindow.draw(debugSprite);
 
         gameWindow.display();
 
     }
+
 
     return 0;
 }
--- window.h
+++ window.h
@@ -6,6 +6,10 @@
 #endif //MINESWEEPER_WINDOW_H
 #include <iostream>
 using namespace std;
+#include <cstdlib>
+#include <ctime>
+#include <random>
+
 #include <SFML/Graphics.hpp>
 
 
@@ -16,34 +20,222 @@ public:
     string inputText;//welcome screen input
     static void keyboardInputName(sf::Event event, sf::Text &text2);
 
-   // void generateMines(int minesRemaining, std::vector<sf::Vector2i> &minePositions);
+
 };
-enum class TileState { Hidden, Revealed, Flagged,Number };
+
+
+
 class Tile{
-public:
-    bool isMine;
+public://note: tile sprite is all sprites
+    bool isMine = false;
+    bool isFlag = false;
+    bool isRevealed = false;
     int adjacentMines;
-    TileState state;
-    Tile(bool isMine = false) : isMine(isMine), adjacentMines(0), state(TileState::Hidden) {}
+    int count;
+    sf::Sprite tileSprite;
+    sf::Sprite tile2sprite;
+    sf::Texture texturehiddentile;
+    sf::Texture revealedTile;
+    sf::Texture flag2;
+    sf::Texture mine2;
+
+
+    Tile(int x, int y, bool isMine = false){
+        texturehiddentile.loadFromFile("files/images/tile_hidden.png");
+        tileSprite.setTexture(texturehiddentile);
+        tileSprite.setPosition(x,y);
+
+        flag2.loadFromFile("files/images/flag.png");
+        tile2sprite.setTexture(flag2);
+        tile2sprite.setPosition(x,y);
+
+        mine2.loadFromFile("files/images/mine.png");
+    }
+
+
+   void drawTile( sf::RenderWindow &gameWindow) {
+       gameWindow.draw(tileSprite);
+       if (isFlag) {
+           gameWindow.draw(tile2sprite);
+       }
+       if (isRevealed and isMine) {
+           tile2sprite.setTexture(mine2);
+           gameWindow.draw(tile2sprite);
+       }
+   }
+
+    void reveal() {
+        revealedTile.loadFromFile("files/images/tile_revealed.png");
+        tileSprite.setTexture(revealedTile);
+        isRevealed = true;
+    }
+    void generateMines(sf::RenderWindow& gameWindow,  vector<vector<Tile>>& tileVector,int mineCount) {
+        random_device rd;
+        mt19937 gen(rd());
+        int minesPlaced = 0;
+        int totalTiles = tileVector.size() * tileVector[0].size();
+
+        while (minesPlaced < mineCount) {
+            int randomIndex = rand() % totalTiles; // Generate random index for the tileVector
+            int row = randomIndex / tileVector[0].size();
+            int col = randomIndex % tileVector[0].size();
+
+            Tile& tile = tileVector[row][col];
+            if (!tile.isMineTile()) {
+                tile.isMine = true;
+                minesPlaced++;
+            }
+        }
+    }
+
+
+
 
     bool isMineTile() const {
+
         return isMine;
     }
-    int getAdjacentMines() const {
-        return adjacentMines;
-    }
-    TileState getState() const {
-        return state;
-    }
-    void setAdjacentMines(int count) {
+    int getAdjacentMines(sf::RenderWindow& gameWindow, const vector<vector<Tile>>& tileVector, int row, int col) {
+        //loops through  adjacent mines
+        //counts them
+        //returns number
+        //if that number is 1-8
+        //then...
+        int count = 0;
+        int numRows = tileVector.size();
+        int numCols = tileVector[0].size();
+        for (int i = row - 1; i <= row + 1; ++i) {
+            for (int j = col - 1; j <= col + 1; ++j) {
+                // Check if neighboring tile is within bounds and not the current tile
+                if (i >= 0 && i < numRows && j >= 0 && j < numCols && !(i == row && j == col)) {
+                    // Check if neighboring tile contains a mine
+                    if (tileVector[i][j].isMineTile()) {
+                        count++;
+                    }
+                }
+            }
+         }
         adjacentMines = count;
+       // cout << count << endl;
+        return count;
+     }
+
+
+
+
+
+
+
+
+
+
+    void revealallMines(sf::RenderWindow& gameWindow,  vector<vector<Tile>>& tileVector) {
+        //user clicks on tile
+        //all other tiles reveal
+        int numRows = tileVector.size();
+        int numCols = tileVector[0].size();
+        for (int i = 0; i < numRows; ++i) {
+            for (int j = 0; j < numCols; ++j) {
+                // Check if the tile is a mine
+                if (tileVector[i][j].isMineTile()) {
+                    // Reveal the mine tile
+                    tile2sprite.setTexture(mine2);
+                    tileVector[i][j].isRevealed = true;
+                }
+            }
+        }
     }
-    void reveal() {
-        state = TileState::Revealed;
+    void revealallTiles(sf::RenderWindow& gameWindow,  vector<vector<Tile>>& tileVector){
+        int numRows = tileVector.size();
+        int numCols = tileVector[0].size();
+        for (int i = 0; i < numRows; ++i) {
+            for (int j = 0; j < numCols; ++j) {
+                // Check if the tile is a mine
+                if (!tileVector[i][j].isMineTile() and !isRevealed) {
+                    // flip all of the hidden tiles to reveal tiles?
+                    tileSprite.setTexture(revealedTile);
+                    tileVector[i][j].isRevealed = true;
+                }
+            }
+        }
     }
-    void toggleFlag() {
-        state = (state == TileState::Flagged) ? TileState::Hidden : TileState::Flagged;
+
+
+
+    void revealNumbers(sf::RenderWindow& gameWindow, int count, vector<vector<Tile>>& tileVector, int x, int y){
+        sf::Texture* numberTexture = new sf::Texture();
+        Tile& tile = tileVector[x][y];
+        if(count >= 1 && count <= 8 && !tile.isMineTile()){//a number and not a mine tile
+            numberTexture->loadFromFile("files/images/number_" + std::to_string(count) + ".png");
+
+        }
+        sf::Sprite* numberSprite = new sf::Sprite();
+        numberSprite->setTexture(*numberTexture);
+        numberSprite->setPosition(x * 32, y * 32);
+
+        // Draw sprite
+        gameWindow.draw(*numberSprite);
+
+        // Clean up memory
+        delete numberSprite;
+        delete numberTexture;
     }
+
+    void openLeaderboard(sf::RenderWindow& gameWindow , sf::RenderWindow& leaderboardWindow){
+        gameWindow.clear(sf::Color(255, 255, 255, 128));
+        leaderboardWindow.clear(sf::Color::Blue);
+        leaderboardWindow.display();
+
+    }
+};
+
+
+class Player{//for leaderboard window
+public:
+    string name;
+    string time;
+
+    static vector<Player> readLeaderboardFromFile(const std::string& filename){
+        ifstream file("leaderboard.txt");
+        vector<Player> leaderboard;
+        if (file.is_open()){
+            string line;
+            while (getline(file, line)){
+                Player player;
+                string rank, timeAndName;
+                size_t pos = line.find(',');
+                if (pos != string::npos) {
+                    rank = line.substr(0, pos);
+                    timeAndName = line.substr(pos + 1); // Extract time and name part
+                    size_t pos2 = timeAndName.find(',');
+
+                    if (pos2 != string::npos) {
+                        player.time = timeAndName.substr(0, pos2);
+                        player.name = timeAndName.substr(pos2 + 1);
+                        leaderboard.push_back(player);
+                    } else {
+                        cout << "Error: Second comma not found in line: " << line << endl;
+                    }
+                }
+                else{
+                    cout << "Error: Comma not found in line: " << line << endl;
+                }
+            }
+            file.close();
+        }
+        return leaderboard;
+    }
+
+
+
+    static void displayLeaderboard(const std::vector<Player>& leaderboard) {
+        int rank = 1;
+        for (const auto& player : leaderboard) {
+            cout << rank << ". " << player.time << " " << player.name << endl;
+            rank++;
+        }
+    }
+
 
 
 
